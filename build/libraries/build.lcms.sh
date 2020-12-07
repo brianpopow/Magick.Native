16d3f867fc30612c67cb70bfb1862e7cb50a73b9
Y56380X <Y56380X@users.noreply.github.com>
2020-12-07 16:34:09 +0100

    adjusted build scripts

--- build/libraries/build.lcms.sh
+++ build/libraries/build.lcms.sh
@@ -3,5 +3,5 @@ set -e
 
 cd lcms
 autoreconf -fiv
-$CONFIGURE --disable-shared --prefix=/usr/local CFLAGS="$FLAGS"
+$CONFIGURE --disable-shared --prefix=/usr/local CFLAGS="$FLAGS" $CROSS_COMPILE_FLAGS
 $MAKE install
